fa74bbae454d3320a41861e1b63551d9c93eb2eb
jos-cabrera <joseacabrera@protonmail.com>
2024-11-30 09:29:03 -0300

    Improve graphics and clean code

--- index.js
+++ index.js
@@ -1,63 +1,79 @@
-let num1 = 0, num2 = 0, operator;
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+let lastOperation = "";
 
-document.getElementById("bar-content").disabled = true;
-document.getElementById("bar-content").value = 0;
+const barContent = document.getElementById("bar-content");
+barContent.disabled = true;
+barContent.value = 0;
 
 function insertNumber(num) {
-  if(document.getElementById("bar-content").value != "0") {
-    document.getElementById("bar-content").value += num;
-  } else {
-    if(num != 0) {
-      document.getElementById("bar-content").value = num;
-    }
-  }
+	if (lastOperation === "=") {
+		barContent.value = num;
+		lastOperation = "";
+	} else if (barContent.value !== "0") {
+		barContent.value += num;
+	} else if (num !== 0) {
+		barContent.value = num;
+	}
 }
 
 function insertOperator(op) {
-  switch(op) {
-    case "+":
-    case "-":
-    case "*":
-    case "/":
-      num1 = document.getElementById("bar-content").value;
-      operator = op;
-      document.getElementById("bar-content").value = "0";
-      break;
-    
-    case ".":
-      document.getElementById("bar-content").value += op;
-      break;
+	switch (op) {
+		case "+":
+		case "-":
+		case "*":
+		case "/":
+			num1 = barContent.value;
+			operator = op;
+			barContent.value = "0";
+			break;
+		case ".":
+			if (!barContent.value.includes(".")) {
+				barContent.value += op;
+			}
+			break;
+		case "C":
+			resetCalculator();
+			break;
+		case "CE":
+			barContent.value = "0";
+			break;
+		case "+/-":
+			barContent.value = String(Number(barContent.value) * -1);
+			break;
+		case "=":
+			calculateResult();
+			break;
+	}
+}
 
-    case "C":
-      document.getElementById("bar-content").value = "0";
-      num1 = 0;
-      num2 = 0;
-      break;
+function resetCalculator() {
+	barContent.value = "0";
+	num1 = 0;
+	num2 = 0;
+	operator = "";
+	lastOperation = "";
+}
 
-    case "CE":
-      document.getElementById("bar-content").value = "0";
-      break;
+function calculateResult() {
+	lastOperation = "=";
+	num2 = barContent.value;
+	const result = performOperation(Number(num1), Number(num2), operator);
+	if (result !== undefined) {
+		barContent.value = result;
+	}
+}
 
-    case "+/-":
-      document.getElementById("bar-content").value*=-1;
-      break;
-
-    case "=":
-      num2 = document.getElementById("bar-content").value;
-      switch(operator) {
-        case "+":
-          document.getElementById("bar-content").value = Number(num1) + Number(num2);
-          break;
-        case "-":
-          document.getElementById("bar-content").value = Number(num1) - Number(num2);
-          break;
-        case "*":
-          document.getElementById("bar-content").value = Number(num1) * Number(num2);
-          break;
-        case "/":
-          document.getElementById("bar-content").value = Number(num1) / Number(num2);
-          break;
-      }
-      break;
-  }
+function performOperation(n1, n2, op) {
+	switch (op) {
+		case "+":
+			return n1 + n2;
+		case "-":
+			return n1 - n2;
+		case "*":
+			return n1 * n2;
+		case "/":
+			return n2 !== 0 ? n1 / n2 : "Error";
+	}
 }
